fcd15927a7c511b1e87cbb463d2a588e88e3bfd3
michel <mkubesch@gmail.com>
2018-11-26 21:28:25 +0100

    changed layout, added navbar

--- webblog/tests.py
+++ webblog/tests.py
@@ -5,4 +5,4 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_cover_view(self):
         response = self.client.get('/')
-        self.assertTemplateUsed(response, 'webblog/cover.html')
+        self.assertTemplateUsed(response, 'webblog/home.html')
